13b63c739c591ffb13dbe413f782e353f01f612f
Winston Weinert <winston@ml1.net>
2020-12-02 17:31:58 -0600

    paramaterize 2020 day01

--- 2020/01/day01.rkt
+++ 2020/01/day01.rkt
@@ -4,11 +4,11 @@ TODO Memoize or at least use prefixes to eliminate duplicate work.
 
 #lang racket
 
-(define (part1 ls)
-  (foldl * 1 (for/first ([c (combinations ls 2)] #:when (= 2020 (foldl + 0 c))) c)))
+(define ((solution N) ls)
+  (foldl * 1 (for/first ([c (combinations ls N)] #:when (= 2020 (foldl + 0 c))) c)))
 
-(define (part2 ls)
-  (foldl * 1 (for/first ([c (combinations ls 3)] #:when (= 2020 (foldl + 0 c))) c)))
+(define part1 (solution 2))
+(define part2 (solution 3))
 
 (module+ main
   (define ls (port->list read))
