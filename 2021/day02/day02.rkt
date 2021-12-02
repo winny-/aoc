a9fe2ea4cded7ea16c9043d101fb2fa58bedb8b2
Winston Weinert <winston@ml1.net>
2021-12-01 23:18:44 -0600

    day02
    Kind of hacky but tried out some different patterns.

---
+++ 2021/day02/day02.rkt
@@ -0,0 +1,42 @@
+#lang racket
+
+(struct Instruction [what mag] #:transparent)
+
+(define (read-instruction [ip (current-input-port)])
+  (match (read)
+    [(? eof-object? e) e]
+    [what (match (read)
+            [(? eof-object? e) e]
+            [mag (Instruction what mag)])]))
+
+(define (part1 ls)
+  (for/fold ([hor 0]
+             [dep 0]
+             #:result (* hor dep))
+            ([i ls])
+    (match-define (struct Instruction (what msg)) i)
+    (match what
+      ['forward (values (+ msg hor) dep)]
+      ['up (values hor (- dep msg))]
+      ['down (values hor (+ dep msg))])))
+
+(define (part2 ls)
+  (for/fold ([hor 0]
+             [dep 0]
+             [aim 0]
+             #:result (* hor dep))
+            ([i ls])
+    (match-define (struct Instruction (what msg)) i)
+    (match what
+      ['forward (values (+ msg hor) (+ dep (* aim msg)) aim)]
+      ['up (values hor dep (- aim msg))]
+      ['down (values hor dep (+ aim msg))])))
+
+(module+ main
+  (define ls (port->list read-instruction))
+  (part1 ls)
+  (part2 ls))
+
+;; Local Variables:
+;; compile-command: "racket day02.rkt < sample.txt"
+;; End:
