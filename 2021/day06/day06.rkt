02e48c6aac7ccb78de660ebb70c99597fe4ee988
Winston Weinert <winston@ml1.net>
2021-12-05 23:40:01 -0600

    2021 Day 6 + rewritten so it's efficient

--- 2021/day06/day06.rkt
+++ 2021/day06/day06.rkt
@@ -1,38 +1,40 @@
 #lang racket
 
+(require racket/hash)
+
 (define (read2 ip)
   (match (read-line ip)
     [(? eof-object? e) e]
-    [s (map string->number (string-split s ","))]))
+    [s (for/fold ([acc (hash)])
+                 ([n (map string->number (string-split s ","))])
+         (hash-update acc n add1 (const 0)))]))
 
 (define OLD 6)
-(define (age ls)
-  (for/fold ([acc empty]
-             #:result (reverse acc))
-            ([lanternfish ls])
-    (match lanternfish
-      [0 (list* (+ 2 OLD) OLD acc)]
-      [_ (cons (sub1 lanternfish) acc)])))
+(define (step pool)
+  (for/fold ([acc (hash)])
+            ([(age count) (in-hash pool)])
+    (match age
+      [0 (hash-union acc (hash OLD count
+                               (+ 2 OLD) count))]
+      [_ (hash-update acc (sub1 age)  (curry +  count) (const 0))])))
 
 
-(define (go ls days)
-  (for/fold ([pool ls]
-             #:result (length pool))
+(define (go input days)
+  (for/fold ([pool input]
+             #:result (for/sum ([n (in-hash-values pool)]) n))
             ([day (in-range 1 (add1 days))])
-    (printf "age: ~a\n" day)
-    (age pool)))
+    (step pool)))
 
-(define (part1 ls)
-  (go ls 80))
+(define (part1 input)
+  (go input 80))
 
-(define (part2 ls)
-  (go ls 256))
+(define (part2 input)
+  (go input 256))
 
 (module+ main
-  (define ls (read2 (current-input-port)))
-  (displayln ls)
-  (part1 ls)
-  (part2 ls))
+  (define input (read2 (current-input-port)))
+  (part1 input)
+  (part2 input))
 
 ;; Local Variables:
 ;; compile-command: "racket day06.rkt < sample.txt"
