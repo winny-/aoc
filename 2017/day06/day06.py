bdecdce9779e26bf4552cb94eed9d936c38293ce
Winston Weinert <git@winny.tech>
2024-11-23 23:32:10 -0600

    solve(2017, day 6): minimal python

---
+++ 2017/day06/day06.py
@@ -0,0 +1,34 @@
+import sys
+import time
+
+
+def redistribute(banks):
+    banks = list(banks)
+    index, blocks = max(enumerate(banks),
+                        key=lambda a: (a[1], len(banks)-a[0]))
+    banks[index] = 0
+    if blocks > len(banks):
+        each = blocks // len(banks)
+        banks = [each+b for b in banks]
+        blocks -= each * len(banks)
+    for i in range(1, blocks+1):
+        banks[(index+i) % len(banks)] += 1
+    return banks
+
+
+if __name__ == '__main__':
+    banks = [int(blocks) for blocks in sys.stdin.readline().split('\t')]
+    seen = {tuple(banks): 0}
+    i = 0
+    while True:
+        i += 1
+        banks = redistribute(banks)
+        t = tuple(banks)
+        if t in seen:
+            print(i)
+            print(i-seen[t])
+            exit()
+        seen[t] = i
+        # print(banks, seen, i)
+        # time.sleep(.5)
+
